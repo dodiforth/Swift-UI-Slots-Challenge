ac1bfd66ef0e6cb2053ad124baf616b7b8ae5ceb
Dowon Kim <dowoniscool@yahoo.com>
2021-02-01 01:54:22 +0900

    Initial Commit

---
+++ Shared/Swift_UI_Slots_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_Slots_ChallengeApp.swift
+//  Shared
+//
+//  Created by dodi on 2021/02/01.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_Slots_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
