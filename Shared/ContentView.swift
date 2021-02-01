87a6572960ee59342ab4bd7368be3e4d11b4acda
Dowon Kim <dowoniscool@yahoo.com>
2021-02-01 11:34:41 +0900

    Build the UI construction

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+                        
+            VStack {
+                
+                Spacer()
+                
+                Text("SwiftUI Slots !")
+                    .font(.largeTitle)
+                    
+                Spacer()
+                
+                HStack {
+                    Text("Credits:")
+                        .fontWeight(.thin)
+                    Text(String(1000))
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    Image("apple").resizable().aspectRatio(contentMode: .fit)
+                    Spacer()
+                    Image("cherry").resizable().aspectRatio(contentMode: .fit)
+                    Spacer()
+                    Image("star").resizable().aspectRatio(contentMode: .fit)
+                    Spacer()
+                    
+                }
+            
+                
+                Spacer()
+                
+                Button("Spin") {
+                    
+                    print("Button is clicked")
+                    
+                }
+                .padding()
+                .padding([.leading,.trailing],40.0)
+                .background(Color.pink)
+                .cornerRadius(25.0)
+                .foregroundColor(.white)
+                .font(.system(size: 30,weight: .bold, design: .default ))
+                
+                Spacer()
+                
+            }
+        }
+        
+        
     }
 }
 
